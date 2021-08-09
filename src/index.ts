2e203e34a45ba80d7c57f8a56f1f0447a1e50df5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-08-09 19:45:14 -0400

    added the server code as well as the dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // tslint:disable:no-var-requires
 require = require('esm')(module);
-module.exports = require('./main.ts');
+module.exports = require(`./main.${
+  process.env.NODE_ENV === 'production' ? 'js' : 'ts'
+}`);
 // tslint:enable:no-var-requires
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,92 @@
-import { Client } from 'tplink-smarthome-api';
+import express from 'express';
+import { Client, Device, Sysinfo } from 'tplink-smarthome-api';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+interface ExtractedDeviceDetails {
+  host: Device['host'];
+  alias: Sysinfo['alias'];
+}
+
+const wrap =
+  (fn: express.RequestHandler) =>
+  (...args: Parameters<express.RequestHandler>) =>
+    Promise.resolve(fn(...args)).catch(args[2]);
+
+const app = express();
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(helmet());
+
+const port = process.env.PORT || process.argv[2] || 8080;
 
 const client = new Client();
 
-client.startDiscovery().on('device-new', (device) => {
+const devices: Map<string, ExtractedDeviceDetails> = new Map();
+
+client
+  .on('device-new', async (device: Device) => {
+    const deviceInfo = await device.getSysInfo();
+
+    if (!devices.has(deviceInfo.deviceId)) {
+      devices.set(deviceInfo.deviceId, {
+        host: device.host,
+        alias: deviceInfo.alias,
+      });
+    }
+  })
+  .startDiscovery();
+
+const intervalHandler = setInterval(() => {
   // tslint:disable-next-line:no-console
-  device.getSysInfo().then(console.log);
+  console.log('Restarting discovery');
+  client.stopDiscovery();
+  client.startDiscovery();
+}, 5 * 60 * 1000);
+
+app.get('/', (_, res) => {
+  res.sendStatus(200);
+});
+
+app.get(
+  '/devices',
+  wrap((_, res) => {
+    res.json(
+      [...devices.entries()].map(([id, { host, alias }]) => ({
+        id,
+        host,
+        alias,
+      }))
+    );
+  })
+);
+
+app.post(
+  '/device-toggle',
+  wrap(async (req, res) => {
+    try {
+      const { hostAddress } = req.body;
+      const device = await client.getDevice({ host: hostAddress });
+      const result = await device.togglePowerState();
+      res.send(result);
+    } catch {
+      res.sendStatus(500);
+    }
+  })
+);
+
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Server running on ${port}`);
+});
+
+process.on('exit', () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Gracefully shutting down server`);
+  clearInterval(intervalHandler);
+  client.stopDiscovery();
+});
+
+process.on('SIGINT', () => {
+  process.exit(0);
 });
